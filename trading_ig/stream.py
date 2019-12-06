742d556bf473c1bb3fb464be1f8b03ccd0bd7372
Hodei Goncalves <hodei.goncalves@gmail.com>
2019-12-06 16:20:39 +0000

    Changes to add subscription on trade events.

--- trading_ig/stream.py
+++ trading_ig/stream.py
@@ -6,10 +6,17 @@ from __future__ import absolute_import, division, print_function
 import sys
 import traceback
 import logging
+import nnpy
+import dill
+from concurrent import futures
+import json
 
-from .lightstreamer import LSClient
+from .lightstreamer import LSClient, Subscription
 
 logger = logging.getLogger(__name__)
+CHANNELS = {
+    "trade": 'inproc://trade_events'
+}
 
 
 class IGStreamService(object):
@@ -38,11 +45,45 @@ class IGStreamService(object):
                                   user=accountId, password=ls_password)
         try:
             self.ls_client.connect()
-            return
         except Exception:
             logger.error("Unable to connect to Lightstreamer Server")
             logger.error(traceback.format_exc())
             sys.exit(1)
+
+        # Create subsciption channel for trade events
+        subscription = Subscription(
+            mode="DISTINCT",
+            items=["TRADE:%s" % accountId],
+            fields=["CONFIRMS", "OPU", "WOU"])
+        pub = nnpy.Socket(nnpy.AF_SP, nnpy.PUB)
+        pub.bind(CHANNELS['trade'])
+
+        def on_item_update(item_update):
+            pub.send(dill.dumps(item_update))
+
+        subscription.addlistener(on_item_update)
+        self.ls_client.subscribe(subscription)
+
+    def wait(self, find_function):
+        def subscribe():
+            sub = nnpy.Socket(nnpy.AF_SP, nnpy.SUB)
+            sub.connect(CHANNELS['trade'])
+            sub.setsockopt(nnpy.SUB, nnpy.SUB_SUBSCRIBE, '')
+            order = None
+            while order is None:
+                data = dill.loads(sub.recv())
+                for value in data.get('values', {}).values():
+                    if value is not None:
+                        value = json.loads(value)
+                        if find_function(value):
+                            order = value
+                            break
+            sub.close()
+            return order
+
+        executor = futures.ThreadPoolExecutor()
+        future = executor.submit(subscribe)
+        return future
 
     def unsubscribe_all(self):
         # To avoid a RuntimeError: dictionary changed size during iteration
